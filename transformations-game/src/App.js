0971c360907bbbbb7c07bba44daafaff359ba5d7
Siliang Chang <siliang.chang@gmail.com>
2020-01-16 20:46:04 -0700

    basic grid setup

--- transformations-game/src/App.js
+++ transformations-game/src/App.js
@@ -1,59 +1,12 @@
-import React, { useState } from 'react';
-import Grid from './components/Grid.js';
-import './App.css';
+import React from "react";
+import Canvas from "./components/Canvas";
 
 const App = () => {
-  let [moveDesc, setMoveDesc] = useState("");
-  let [moveType, setMoveType] = useState("Select");
-
-  const handleSubmit = (event) => {
-    console.log(moveDesc, moveType);
-    setMoveDesc(moveDesc = "");
-    setMoveType(moveType = "Select");
-    event.preventDefault();
-  };
-
   return (
-    <div className="App">
-      <div className="Left-Menu-container">
-        LEFT MENU
-        <br />
-        Move Description:
-        <form onSubmit={(event) => handleSubmit(event)}>
-          {/* May require more move inputs for the demo */}
-          <input
-            className="Moves-Input"
-            type="text"
-            placeholder="move desc."
-            name="moveDesc"
-            value={moveDesc}
-            onChange={(event) => setMoveDesc(event.target.value)} />
-          <br />
-          Move Type:
-        <select
-            className="Moves-Dropdown"
-            name="themeValue"
-            value={moveType}
-            onChange={(event) => setMoveType(event.target.value)}
-          >
-            <option value="Select">-- Select Move --</option>
-            <option value="Translation">Translation</option>
-            <option value="Rotation">Rotation</option>
-            <option value="Reflection">Reflection</option>
-          </select>
-          <br />
-          <input
-            className="Moves-Submit"
-            type="submit"
-            value="Submit Move"
-          />
-        </form>
-      </div>
-      <div className="Right-Grid-container">
-        < Grid />
-      </div>
+    <div /*onClick={e => console.log("x: ", e.clientX, "y: ", e.clientY)}*/>
+      <Canvas />
     </div>
   );
-}
+};
 
-export default App;
+export default App
---
+++ transformations-game/src/components/settings.js
@@ -0,0 +1,21 @@
+// Grid size
+export const SIZE = 600;
+// Grid to border margin
+export const GRID_MARGIN = SIZE * 0.025;
+// Units for x, y axis
+export const DIMENSION = 20;
+const unit = Math.floor(SIZE / DIMENSION);
+// Shape of object (not limited to triangle), points coords in counter-clock wise order
+// (iX, iY) is the first point of the object, defaut setting: a 3-4-5 triangle
+export const triangle_shape = (iX, iY) => {
+  return {
+    x1: iX,
+    y1: iY,
+    x2: iX,
+    y2: iY + unit * 3,
+    x3: iX + unit * 4,
+    y3: iY + unit * 3
+  };
+};
+
+export const AXIS_LABEL_SIZE = "0.6em";
