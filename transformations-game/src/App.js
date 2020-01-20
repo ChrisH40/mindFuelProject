8269a91cf8f8e77a28ada319ade7f06318563dba
ChrisH40 <56125851+ChrisH40@users.noreply.github.com>
2020-01-20 15:37:47 -0700

    updated css

--- transformations-game/src/App.js
+++ transformations-game/src/App.js
@@ -2,7 +2,22 @@ import React from "react";
 import LevelOne from "./components/LevelOne";
 
 const App = () => {
-  return <LevelOne />;
+
+  return (
+    <div className="App">
+      <div className="Canvas">
+        <LevelOne />
+      </div>
+      <div className="Right-Container">
+        <div className="Top-Container">
+          Top Menu
+        </div>
+        <div className="Bottom-Container">
+          Bottom Menu
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default App
